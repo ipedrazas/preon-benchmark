7714489ea30bbae087620c278a208ddbf5cb6022
Ivan Pedrazas <ipedrazas@gmail.com>
2015-07-17 07:47:57 +0100

    commandline parser added

--- hazelcast/src/main/java/me/pedrazas/benchmark/hazelcast/BenchMap.java
+++ hazelcast/src/main/java/me/pedrazas/benchmark/hazelcast/BenchMap.java
@@ -3,6 +3,12 @@ package me.pedrazas.benchmark.hazelcast;
 import java.util.Random;
 import java.util.concurrent.ConcurrentMap;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
@@ -13,11 +19,33 @@ public class BenchMap {
 	private static Random randomGenerator = new Random();
 	
 	public static void main(String[] args) {
+		
+		CommandLineParser parser = new DefaultParser();
+		Options options = new Options();
+		options.addOption( "h", "hazelcast", false, "Hazelcast connection" );
+		options.addOption( "a", "objects-added", false, "Number of objects added" );
+		String hazelcastUrl = "localhost:5701";
+		int numObjects = 1000;
+		try {
+		    // parse the command line arguments
+		    CommandLine line = parser.parse( options, args );
+
+		    if( line.hasOption( "hazelcast" ) ) {
+		        hazelcastUrl = line.getOptionValue("hazelcast");
+		    }
+		    if( line.hasOption( "objects-added" ) ) {
+		    	numObjects = Integer.parseInt(line.getOptionValue("objects-added"));
+		    }
+		}
+		catch( ParseException exp ) {
+		    System.out.println( "Unexpected exception:" + exp.getMessage() );
+		}
+		
 		ClientConfig clientConfig = new ClientConfig();
-        clientConfig.addAddress("localhost:5701");
+        clientConfig.addAddress(hazelcastUrl);
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
-        int top = randomGenerator.nextInt(1000);
-        for (int i = 0; i < top; i++) {
+        
+        for (int i = 0; i < numObjects; i++) {
         	write(client);        
             read(client); 
 		}
